b22886432f9ed151f491a4aa59cac5699df71c65
Wang Tianci <wangtianci2004@gmail.com>
2025-10-03 00:33:44 +0800

    Feat: Homepage

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple script for Yitong Lin's homepage
+// Minimal functionality - keeping it clean and simple
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('👋🏻 Welcome to Tianci Wang\'s homepage!');
+});
